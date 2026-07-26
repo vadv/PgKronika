02e2dc7e0ce4cf7b860e3bfd13b2dfdf14782b9a
dmitrivasilyev <vadv.mkn@gmail.com>
2026-07-26 11:16:29 +0300

    test(bdd): strip host xattrs from source archives

--- scripts/bdd-image.sh
+++ scripts/bdd-image.sh
@@ -134,7 +134,7 @@ source_tar() {
     cd "$root"
     git ls-files -co --exclude-standard -z -- "$@" \
       | LC_ALL=C sort -z \
-      | tar --null -T - -cf -
+      | tar --no-xattrs --null -T - -cf -
   )
 }
 
